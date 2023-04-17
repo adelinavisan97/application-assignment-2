ebb706efca409500f3d414eb997898bab542aae9
adelina.visan <adelina.visan@vodafone.com>
2023-04-17 14:23:56 +0100

    fix: :recycle: adding comments

--- api/server.js
+++ api/server.js
@@ -1,15 +1,18 @@
+//Importing dependencies
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 
+//Creating an instance of the Express.js application
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 
+//Connecting to a MongoDB database using a connection string and setting some options (useNewUrlParser and useUnifiedTopology) to avoid deprecation warnings.
 mongoose.connect("mongodb+srv://visanadelina:abc123.@cluster0.azypm3a.mongodb.net/AssignmentDatabase?appName=mongosh+1.8.0", {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -17,14 +20,17 @@ mongoose.connect("mongodb+srv://visanadelina:abc123.@cluster0.azypm3a.mongodb.ne
     .then(() => console.log("Connected to DB"))
     .catch(console.error);
 
+// Importing the People model, which defines the schema for the "people" document in MongoDB
 const People = require('./models/model');
 
+//Creating a route that handles GET requests to the "/people" endpoint. It retrieves all documents from the "people" collection in MongoDB and returns them in JSON format
 app.get('/people', async (req, res) => {
     const people = await People.find();
 
     res.json(people);
 })
 
+//
 app.post('/new-entry', (req, res) => {
     const entry = new People({
         name: req.body.name,
